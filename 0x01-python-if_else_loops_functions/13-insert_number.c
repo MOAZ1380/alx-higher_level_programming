a49c5706ab338e5e4c4e69fe6c35237e19cd276f
moaz <moaz469654@gmail.com>
2024-03-22 06:06:50 -0700

    10

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * insert_node - I
+ * @head: Point
+ * @number: Num
+ * Return: A
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return NULL;
+	}
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (*head == NULL || number < (*head)->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return new_node;
+	}
+	listint_t *current = *head;
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return new_node;
+}
