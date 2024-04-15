de93b01afbae0637ad69751ba3212d4132dc1b38
moaz <moaz469654@gmail.com>
2024-04-15 15:07:24 -0700

    moaz sayed

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-"""Defines a function that adds attributes to objects."""
+"""
+    11-square: class Square from Rectangle
+"""
 
 
-def add_attribute(obj, att, value):
-    """Add a new attribute to an object if possible.
+Rectangle = __import__('9-rectangle').Rectangle
 
-    Args:
-        obj (any): The object to add an attribute to.
-        att (str): The name of the attribute to add to obj.
-        value (any): The value of att.
-    Raises:
-        TypeError: If the attribute cannot be added.
+
+class Square(Rectangle):
     """
-    if not hasattr(obj, "__dict__"):
-        raise TypeError("can't add new attribute")
-    setattr(obj, att, value)
+        Square  inherits from Rectangle
+        Attributes:
+            size (int): side of square
+        Methods:
+            __init__ - initialises the square
+    """
+    def __init__(self, size):
+        """
+            initialises Square
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        """
+            Returns the area of square
+        """
+        area = self.__size * self.__size
+        return area
+
+    def __str__(self):
+        return ("[{}] {}/{}".format(type(self).__name__,
+                                    self.__size, self.__size))
