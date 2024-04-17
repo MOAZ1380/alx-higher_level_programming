fed9d9cc51088449d4861d13416cd532c8f7ff8a
moaz <moaz469654@gmail.com>
2024-04-16 17:50:29 -0700

    m c

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
+
 """
-script to save and load
+Write a script that adds all arguments to a Python list
+and then save them to a file:
+- You must use your function save_to_json_file from 5-save_to_json_file.py
+- You must use your function load_from_json_file from 6-load_from_json_file.py
+- The list must be saved as JSON representation in a file named add_item.json
+- If the file doesn’t exist, it should be created
+- You don’t need to manage file permissions / exceptions.
 """
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-
-filename = 'add_item.json'
-my_list = []
 try:
-    my_list = load_from_json_file(filename)
-except Exception:
-    save_to_json_file(my_list, filename)
-
-arg_len = len(argv)
-
-if arg_len > 1:
-    for i in range(1, arg_len):
-        my_list.append(argv[i])
-
-    save_to_json_file(my_list, filename)
+    read = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    read = []
+read.extend(sys.argv[1:])
+save_to_json_file(read, "add_item.json")
