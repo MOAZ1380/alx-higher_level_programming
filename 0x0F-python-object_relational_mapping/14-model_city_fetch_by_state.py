e84ca544e119eba06e8182da24dd6490164442d0
moaz <moaz469654@gmail.com>
2024-06-22 11:27:59 -0700

    ,

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""prints all City objects
-from the database hbtn_0e_14_usa"""
-
+"""
+List all State objects with a
+"""
 if __name__ == "__main__":
-
     import sys
     from model_state import Base, State
     from model_city import City
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
+    from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(mysql.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    for state, city in session.query(State, City)\
-                              .filter(City.state_id == State.id)\
-                              .order_by(City.id).all():
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State, City).filter(State.id == City.state_id)
+    for s, c in states.order_by(City.id).all():
+        print("{:s}: ({:d}) {:s}".format(s.name, c.id, c.name))
