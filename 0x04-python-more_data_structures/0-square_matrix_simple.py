1c1b45f6c0da754229cbcc5cdcd85538950bd2c0
Moaz Sayed <moaz469654@gmail.com>
2024-08-10 18:15:54 +0200

    j

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-    for i in matrix:
-        squared_row = []
-        for y in i:
-            squared_row.append(y ** 2)
-        squared_matrix.append(squared_row)
-    return squared_matrix
--- 0x10-python-network_0/3-methods.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Write a Bash script that takes in a URL and displays all HTTP methods the server
-curl -sI -X OPTIONS "$1"
