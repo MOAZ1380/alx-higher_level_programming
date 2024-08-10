6de5df878c0e331df1de6598897dcf60d64e095d
moaz <moaz469654@gmail.com>
2024-08-10 14:25:20 -0700

    d

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+const request = require("request");
+let url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
 });
