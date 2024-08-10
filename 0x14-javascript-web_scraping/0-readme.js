7e23c3ad27640a5610775e8951a980cbde28346a
Moaz Sayed <moaz469654@gmail.com>
2024-08-10 18:17:13 +0200

    k

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const fs = require('fs');
-const filePath = process.argv[2];
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data);
-    }
+const fs = require("fs");
+fs.readFile(process.argv[2], "utf8", function (error, content) {
+    console.log(error || content);
 });
